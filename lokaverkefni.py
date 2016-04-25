bbb76764d0765803837d14dc1436ea0882ea30bf
Erkur <eiki@live.com>
2016-04-25 00:45:38 +0000

    Update lokaverkefni.py

--- lokaverkefni.py
+++ lokaverkefni.py
@@ -1,131 +1,143 @@
 from tkinter import *
 import tkinter as tk
 import random
+import string
 import time
 
-
+#talninga breytur
+wordsCount = 0
+wordsCorrect = 0
+wordsIncorrect = 0
+keyCount = 0
+timeMin = 1
 
 def main():
     global root
     root=Tk()
     root.title("Typeracer")
-    root.geometry("400x400")   
+    root.geometry("400x400")
+    root.configure(background='#f1ffff')
     typeracer()
     root.mainloop()
 
 def typeracer():
-    global wordsCount
-    global wordsCorrect
-    global wordsIncorrect
-    global keyCount
-    global keyCorrect
-    global keyIncorrect
-    global keyWordCount
-    #talninga breytur
-    wordsCount = 0
-    wordsCorrect = 0
-    wordsIncorrect = 0
-    keyCount = 0
-    keyCorrect = 0
-    keyIncorrect = 0
-    keyWordCount = 0
-
-    def before():
-        #gerir random listann af orðum
-        wordsList = []
-        for i in range(0,9):
-            liner = [line.strip() for line in open('betterWords.txt')]
-            wordsList.append(random.choice(liner))
+    #gerir random listann af orðum
+    wordsList = []
+    for i in range(0,9):
+        liner = [line.strip() for line in open('betterWords.txt')]
+        wordsList.append(random.choice(liner))
         
-        var = tk.StringVar()
-        max_len = 0
-        
-        #sýnir listann sem label
-        w = tk.Label(root, text=wordsList)
-        w.pack()
-        
-        def on_write(*args):
-            s = var.get()
-            if len(s) > max_len:
-                var.set(s[:max_len])
+    #sýnir listann sem label
+    wordListLabel = tk.Label(root, text=wordsList,borderwidth=1, bd=3, relief=GROOVE)
+    wordListLabel.pack()
+    wordListLabel.place(x=80, y=5)
 
-        var.trace_variable("w", on_write)
-        entry = tk.Entry(root, textvariable=var, background='#FF9999')
-        entry.pack()
+    #entry boxið fyrir orðinn
+    textbox = tk.Entry(root, background='#FF9999', state=DISABLED)
+    textbox.pack()
+    textbox.place(x=145, y=30)
 
-        label = tk.Label(root)
-        label.place(x=35, y=15)
-        def countdown(count):
-            label['text'] = count
-            if count > 0:
-                # call countdown again after 1000ms (1s)
-                root.after(1000, countdown, count-1)
-            elif count == 0:
-                typeracer()
-                entry.configure(background="white")
-                label['text'] = "GO!"
-        b = Button(root, text="Start", command=lambda: countdown(3))
-        b.pack()
-    before()
+    #countdown til byrjunar (3 sec)
+    countdownLabel = tk.Label(root)
+    countdownLabel.place(x=200, y=200)
+    
+    def countdownToStart(count):
+        global max_len
+        startButton.configure(state=DISABLED)
+        countdownLabel['text'] = count
+        if count > 0:
+            # call countdown again after 1000ms (1s)
+            root.after(1000, countdownToStart, count-1)
+        elif count == 0:
+            textbox.configure(background="white", state=NORMAL)
+            textbox.focus()
+            countdownLabel['text'] = "GO!"
+            timer(60)
+
+    startButton = Button(root, text="Start", command=lambda: countdownToStart(3))
+    startButton.pack()
+    startButton.place(x=185, y=50)
+
+    timerLabel = tk.Label(root)
+    timerLabel.place(x=200, y=100)
+
+    def restart():
+        global wordsCount
+        global wordsCorrect
+        global wordsIncorrect
+        global keyCount
+        sec = 0
+        wordsCount = 0
+        wordsCorrect = 0
+        wordsIncorrect = 0
+        keyCount = 0
+        startButton.configure(state=NORMAL)
+            
+    restartButton = Button(root, text="Restart", command=restart())
+    restartButton.pack()
+    restartButton.place(x=250, y=50)
+
+    #telur niður 60 sec
+    
+    def timer(sec):
+        timerLabel['text'] = sec
+        if sec > 0:
+            # call countdown again after 1000ms (1s)
+            root.after(1000, timer, sec-1)
+        elif sec == 0:
+            startButton.configure(state=NORMAL)
+            textbox.configure(background="#FF9999", state=DISABLED)
+            timerLabel['text'] = "TIMER OVER!"
+            showWPM(keyCount, wordsIncorrect, timeMin)
+
+    def showWPM(entries, errors, t):
+        print (((entries/5) - errors) / t)
+        print ("keycount: ", keyCount)
+        print ("incorrect words: ",wordsIncorrect)
+        print ("minutes: ",timeMin)
+    
     #birtir nýtt orð
     def newWord():
         wordsList.pop(0)
         liner = [line.strip() for line in open('betterWords.txt')]
         wordsList.insert(9,random.choice(liner))
-        entry.delete(0, END)
-        w.config(text=wordsList)
+        textbox.delete(0, END)
+        wordListLabel.config(text=wordsList)
 
     #ef orðið er rétt skrifað
     def correctWord(event=None):
         global wordsCount
         global wordsCorrect
-        global keyWordCount
-        keyWordCount = 0
+        global keyCount
+        keyCount += len(wordsList[0]) + 1
         wordsCount += 1
         wordsCorrect += 1
         newWord() 
         print ("Correct")
+        print (keyCount)
 
     #ef orðið er rangt skrifað
     def incorrectWord(event=None):
         global wordsCount
         global wordsIncorrect
-        global keyWordCount
-        keyWordCount = 0
+        global keyCount
+        keyCount += len(textbox.get())
         wordsCount += 1
         wordsIncorrect += 1
         newWord()
         print ("Incorrect")
+        print (keyCount)
 
     #ef ýtt er á space þá fer það í gegnum checkið
     def spacePressed(event=None):
-        if entry.get() == wordsList[0]:
+        global keyCount
+        if textbox.get()[:-1] == wordsList[0]:
             correctWord()
         else:
+            print (wordsList[0])
+            print (textbox.get())
             incorrectWord()
             
-    def click(key):
-        global keyCount
-        global keyCorrect
-        global keyIncorrect
-        global keyWordCount
-        keyCount += 1
-        print (key.char)
-        if key.char == ' ':
-                keyWordCount = 0
-        else:
-            if key.char == wordsList[0][keyWordCount]:
-                keyCorrect += 1
-                keyWordCount += 1
-            else:
-                keyIncorrect += 1
-                keyWordCount += 1
-        print ("Total key:",keyCount)
-        print ("Correct keys:",keyCorrect)
-        print ("Incorrect keys",keyIncorrect)
-        print (keyWordCount)
-        
-    entry.bind("<Key>", click)
     root.bind("<space>", spacePressed)  
     count_flag = True   
 
